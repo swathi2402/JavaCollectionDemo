7250e77fe7c194e2337115ff28687c7072c3093b
Swathi <swathi.h@perfios.com>
2021-09-13 15:46:56 +0530

    ADDED: Stack of collections used to push, pop elements

--- src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
+++ src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
@@ -3,13 +3,14 @@ package com.bridgelabz.collectiondemo;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class JavaCollectionDemo {
 
 	public static void main(String[] args) {
 
 		doListDemo();
-
+		doStackDemo();
 	}
 
 	private static void doListDemo() {
@@ -26,6 +27,22 @@ public class JavaCollectionDemo {
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
+	}
 
+	private static void doStackDemo() {
+		System.out.println("\nIn doStackDemo");
+		Stack<String> stack = new Stack<>();
+		stack.push("Ayush");
+		stack.push("Garvit");
+		stack.push("Amit");
+		stack.push("Ashish");
+		stack.push("Garima");
+
+		stack.pop();
+
+		Iterator<String> itr = stack.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 	}
 }
